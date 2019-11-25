edd311140ff749a9fe101c1472143cf43006e2b2
Joe <JTSenneker>
2019-11-25 09:30:20 -0500

    added an IE folder for WoF to be converted

---
+++ src/WoFIE/src/Scene3.js
@@ -0,0 +1,165 @@
+class Scene3 extends Phaser.Scene {
+
+
+    constructor(handle, parent) {
+        super(handle);
+        this.handle = handle;
+        this.parent = parent;
+        this.WIDTH = 800;
+        this.HEIGHT = 600;
+    }
+    create() {
+        this.cameras.main.setViewport(this.parent.x, this.parent.y, this.WIDTH, this.HEIGHT);
+        this.setupPuzzle(puzzles[0]);
+        this.setupButtons();
+    }
+    refresh() {
+        this.cameras.main.setPosition(this.parent.x, this.parent.y);
+        this.scene.bringToTop();
+    }
+    setupPuzzle(puzzle) {
+        category = puzzle.category;
+        for (var i = 0; i < 14; i++) {
+            row1[i] = this.add.sprite(i * 32, 0, 'wordTile').setOrigin(0, 0);
+            row2[i] = this.add.sprite(i * 32, 64, 'wordTile').setOrigin(0, 0);
+            row3[i] = this.add.sprite(i * 32, 128, 'wordTile').setOrigin(0, 0);
+            row4[i] = this.add.sprite(i * 32, 192, 'wordTile').setOrigin(0, 0);
+            row1[i].letter = " ";
+            row2[i].letter = " ";
+            row3[i].letter = " ";
+            row4[i].letter = " ";
+            this.anims.create({
+                key: 'noLetter',
+                frames: [{ key: 'wordTile', frame: 0 }],
+                frameRate: 20
+            });
+
+            this.anims.create({
+                key: 'letter',
+                frames: [{ key: 'wordTile', frame: 1 }],
+                frameRate: 20
+            })
+        }
+        for (var i = 0; i < 14; i++) {
+
+            if (i > puzzle.row1.length - 1) break;
+            if (puzzle.row1[i] == " ") continue;
+            row1[i].anims.play('letter');
+            row1[i].letter = puzzle.row1[i];
+
+        }
+        for (var i = 0; i < 14; i++) {
+
+            if (i > puzzle.row2.length - 1) break;
+            if (puzzle.row2[i] == " ") continue;
+            row2[i].anims.play('letter');
+            row2[i].letter = puzzle.row2[i];
+
+        }
+        for (var i = 0; i < 14; i++) {
+
+            if (i > puzzle.row3.length - 1) break;
+            if (puzzle.row3[i] == " ") continue;
+            row3[i].anims.play('letter');
+            row3[i].letter = puzzle.row3[i];
+
+        }
+        for (var i = 0; i < 14; i++) {
+
+            if (i > puzzle.row4.length - 1) break;
+            if (puzzle.row4[i] == " ") continue;
+            row4[i].anims.play('letter');
+            row4[i].letter = puzzle.row4[i];
+
+        }
+    }
+
+    setupButtons() {
+        for (var i = 21; i >= 0; i--) {
+            var button = this.add.text(i * 24, 300, consonants[i], { fontSize: '24px', fill: '#FFF' });
+            consonantButtons[i] = button;
+        }
+        for (var i = 5; i >= 0; i--) {
+            var button = this.add.text(i * 24, 324, vowels[i], { fontSize: '24px', fill: '#FFF' });
+            vowelButtons[i] = button;
+        }
+
+        for (var i = consonantButtons.length - 1; i >= 0; i--) {
+            var scene = this;
+
+            consonantButtons[i].setInteractive();
+            consonantButtons[i].on('pointerup', function () {
+                if (!gameOptions.canSelect) return;
+                console.log(this.text);
+                scene.CheckLetter(this.text);
+                this.text = " "
+                gameOptions.canSelect = false;
+            });
+        }
+
+        for (var i = vowelButtons.length - 1; i >= 0; i--) {
+
+            var scene = this;
+
+            vowelButtons[i].setInteractive();
+            vowelButtons[i].on('pointerup', function () {
+                if (!gameOptions.buyingVowel) return;
+
+                scene.CheckLetter(this.text);
+                this.text = " "
+                gameOptions.score -= 250;
+                gameOptions.canSelect = false;
+                gameOptions.buyingVowel = false;
+            });
+        }
+    }
+
+    CheckLetter(letter) {
+
+        for (var j = row1.length - 1; j >= 0; j--) {
+
+            if (row1[j].letter.toUpperCase() == letter) {
+                this.add.text(row1[j].x, row1[j].y, letter, { fontSize: '36px', fill: '#4B4B4B' });
+                if (!this.isVowel(letter)) gameOptions.score += gameOptions.scoreMultiplier;
+            }
+        }
+        for (var j = row2.length - 1; j >= 0; j--) {
+
+            if (row2[j].letter.toUpperCase() == letter) {
+                this.add.text(row2[j].x, row2[j].y, letter, { fontSize: '36px', fill: '#4B4B4B' });
+                if (!this.isVowel(letter)) gameOptions.score += gameOptions.scoreMultiplier;
+            }
+        }
+        for (var j = row3.length - 1; j >= 0; j--) {
+
+            if (row3[j].letter.toUpperCase() == letter) {
+                this.add.text(row3[j].x, row3[j].y, letter, { fontSize: '36px', fill: '#4B4B4B' });
+                if (!this.isVowel(letter)) gameOptions.score += gameOptions.scoreMultiplier;
+            }
+        }
+        for (var j = row4.length - 1; j >= 0; j--) {
+
+            if (row4[j].letter.toUpperCase() == letter) {
+                this.add.text(row4[j].x, row4[j].y, letter, { fontSize: '36px', fill: '#4B4B4B' });
+                if (!this.isVowel(letter)) gameOptions.score += gameOptions.scoreMultiplier;
+            }
+        }
+    }
+    update() {
+        for (var i = consonantButtons.length - 1; i >= 0; i--) {
+            if (!gameOptions.canSelect) {
+                consonantButtons[i].alpha = .25;
+            } else consonantButtons[i].alpha = 1;
+        }
+
+        for (var i = vowelButtons.length - 1; i >= 0; i--) {
+            if (!gameOptions.buyingVowel) {
+                vowelButtons[i].alpha = .25;
+            } else vowelButtons[i].alpha = 1;
+        }
+    }
+
+    isVowel(letter) {
+        return (letter == "A" || letter == "E" || letter == "I" || letter == "O" || letter == "U")
+    }
+}
---
+++ src/WoFIE/src/game.js
@@ -0,0 +1,127 @@
+
+var config = {
+    width: 800,
+    height: 600,
+    backgroundColor: 0x000025,
+    scene: [Scene1]
+}
+const puzzles = [
+    {
+        category: "Phrase",
+        row1: "learning",
+        row2: "wheels in",
+        row3: "motion",
+        row4: ""
+
+    }
+];
+const vowels = ['A','E','I','O','U'];
+const consonants = ['B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z'];
+let row1 = [];
+let row2 = [];
+let row3 = [];
+let row4 = [];
+let consonantButtons = [];
+let vowelButtons = [];
+let category = "";
+const gameOptions = {
+    //configure slices
+    //array of JSON objects
+    slices:[
+        {
+            degrees: 60,
+            startColor: 0xff0000,
+            endColor: 0xff8800,
+            rings: 3,
+            text: "200",
+            score: 200
+        },
+        {
+            degrees: 60,
+            startColor: 0x000000,
+            endColor: 0x008800,
+            rings: 4,
+            text: "300",
+            score: 300
+        },
+        {
+            degrees: 60,
+            startColor: 0x0000ff,
+            endColor: 0x0088ff,
+            rings: 5,
+            text: "400",
+            score: 400
+        },
+        {
+            degrees: 60,
+            startColor: 0x0000ff,
+            endColor: 0x8800ff,
+            rings: 6,
+            text: "100",
+            score: 100
+        },
+        {
+            degrees: 60,
+            startColor: 0xff0000,
+            endColor: 0xff0088,
+            rings: 7,
+            text: "500",
+            score: 500
+        },
+        {
+            degrees: 60,
+            startColor: 0x002200,
+            endColor: 0x00ee22,
+            rings: 8,
+            text: "600",
+            score: 500
+        }
+    ],
+    canSpin: false,
+    canSelect: false,
+    atWheel:false,
+    buyingVowel:false,
+    scoreMultiplier:0,
+    score:0,
+    //wheel rotation duration (ms)
+    rotationTimeRange:{
+        min: 3000,
+        max: 4500
+    },
+    //how many rounds before it stops
+    wheelRounds:{
+        min:2,
+        max:10
+    },
+    //degrees the wheel spins back before it stops
+    backSpin:{
+        min:1,
+        max:4
+    },
+    wheelRadius: 240,
+    strokeColor: 0xffffff,
+    strokeWidth:5
+}
+
+window.onload = function(){
+    var game = new Phaser.Game(config);
+    
+}
+function resize() {
+    var canvas = document.querySelector("canvas");
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    var windowRatio = windowWidth / windowHeight;
+    var gameRatio = game.config.width / game.config.height;
+    if(windowRatio < gameRatio){
+        canvas.style.width = windowWidth + "px";
+        canvas.style.height = (windowWidth / gameRatio) + "px";
+    }
+    else{
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px";
+    }
+}
+
+
+
