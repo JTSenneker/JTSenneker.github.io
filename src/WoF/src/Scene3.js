5a5f023e5d768497a5086ecf2d69591c758cea6c
Joe <JTSenneker>
2019-11-25 13:51:19 -0500

    checking puzzlebox letters

--- src/WoF/src/Scene3.js
+++ src/WoF/src/Scene3.js
@@ -134,6 +134,7 @@ class Scene3 extends Phaser.Scene {
     CheckLetter(letter) {
         if (!gameOptions.solving) {
             for (var j = this.puzzleBoxes.length - 1; j >= 0; j--) {
+                console.log(this.puzzleBoxes[j].letter);
                 if (this.puzzleBoxes[j].resolved) continue;
                 if (this.puzzleBoxes[j].letter.toUpperCase() == letter) {
                     this.add.text(this.puzzleBoxes[j].x, this.puzzleBoxes[j].y, letter, { fontSize: '36px', fill: '#4B4B4B' });
