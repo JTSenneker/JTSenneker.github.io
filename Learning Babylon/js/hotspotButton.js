718ef5035d0ba1f41cb4855ac0eb4b4f82adfce0
JTSenneker <JTSenneker@gmail.com>
2024-01-29 16:51:36 -0500

    removed garbage added pokedex

--- Learning Babylon/js/hotspotButton.js
+++
@@ -1,9 +0,0 @@
-BABYLON.GUI.Button.CreateHotspotButton = function (name, text, image) {
-    var result = new BABYLON.GUI.Button(name);
-
-    //Adding image
-    var iconImage = image;
-    iconImage.stretch = BABYLON.GUI.Image.STRETCH_UNIFORM;
-    result.addControl(iconImage);
-    return result;
-};
